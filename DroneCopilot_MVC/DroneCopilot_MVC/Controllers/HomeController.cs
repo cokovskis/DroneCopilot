4fd3067bfcba10ccb51b31429be9558b7e20f4cd
Andrej Georgievski <andrej.zgeorgievski@gmail.com>
2016-04-23 16:20:01 +0200

    MVC Project now available
    Added the full, compiled MVC project with bing maps API. Development
    phase: building a mock-up for presentation purposes

---
+++ DroneCopilot_MVC/DroneCopilot_MVC/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DroneCopilotServices;
+
+namespace DroneCopilot_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult Settings()
+        {
+            //ViewBag.Message = "Settings";
+            WeatherData x = new WeatherData();
+            ViewBag.Message = x.receiveData();
+
+            return View();
+        }
+
+        public ActionResult Data()
+        {
+            ViewBag.Message = "Numeric view";
+
+            return View();
+        }
+    }
+}
---
+++ DroneCopilot_MVC/DroneCopilot_MVC/Models/Location.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DroneCopilot_MVC.Models
+{
+    public class Location
+    {
+        private string name;
+        private double latitude;
+        private double longitude;
+        private double height;
+
+        public Location()
+        {
+            name = "";
+            latitude = 0.00;
+            longitude = 0.00;
+            height = 20.00;
+        }
+        public Location(double lat, double lon, double h, string name = "")
+        {
+            name = "";
+            latitude = lat;
+            longitude = lon;
+            height = h;
+        }
+
+        void setLatitude(double lat)
+        {
+            latitude = lat;
+        }
+        void setLongitude(double lon)
+        {
+            longitude = lon;
+        }
+        void setHeight(double h)
+        {
+            height = h;
+        }
+
+        double getLatitude()
+        {
+            return latitude;
+        }
+        double getLongitude()
+        {
+            return longitude;
+        }
+        double getHeight()
+        {
+            return height;
+        }
+
+        string toString()
+        {
+            return latitude.ToString() + ", " + longitude.ToString() + " at " + height.ToString() + "m";
+        }
+
+    }
+}
